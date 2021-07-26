0ac366fbd87ed9a4d18348054754036219d10227
momofosho <54734022+momofosho@users.noreply.github.com>
2021-07-26 20:52:41 +0800

    Update home.py

--- home.py
+++ home.py
@@ -258,6 +258,28 @@ def home(state):
 #             count+=
 #         st.write(int(posts_info[posts_info]['likes'].sum(skipna=True)))
 
+
+
+    from bokeh.models.widgets import Panel, Tabs
+    from bokeh.io import output_file, show
+    from bokeh.plotting import figure
+
+    output_file("slider.html")
+
+    p1 = figure(plot_width=300, plot_height=300)
+    p1.circle([1, 2, 3, 4, 5], [6, 7, 2, 4, 5], size=20, color="navy", alpha=0.5)
+    tab1 = Panel(child=p1, title="circle")
+
+    p2 = figure(plot_width=300, plot_height=300)
+    p2.line([1, 2, 3, 4, 5], [6, 7, 2, 4, 5], line_width=3, color="navy", alpha=0.5)
+    tab2 = Panel(child=p2, title="line")
+
+    tabs = Tabs(tabs=[ tab1, tab2 ])
+
+    show(tabs)
+
+
+
     stop_words = set(stopwords.words('english'))
     
     for cap in posts_info["caption"]:
