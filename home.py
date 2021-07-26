a8e162fc89d80a2fa45c5f26c472aae1107c4f74
momofosho <54734022+momofosho@users.noreply.github.com>
2021-07-27 06:01:24 +0800

    Update home.py

--- home.py
+++ home.py
@@ -249,7 +249,12 @@ def home(state):
         'Select hashtag',
         options=hashtag_list
     )
-    st.write(df1[df1[any(tag in "hashtags" for tag in hashtag_filter_multiselect)]])
+    
+    pattern = '|'.join(hashtag_filter_multiselect)
+    st.write(df1[df1.a.str.contains(pattern)])
+    
+    
+#     st.write(df1[df1[any(tag in "hashtags" for tag in hashtag_filter_multiselect)]])
 #     st.write(df1[df1["username"]==state.query_username and (tag in df1["hashtags"] for tag in hashtag_filter_multiselect).any()])
     likes2, comments2, d2 = st.beta_columns(3)
 #     with likes2:
