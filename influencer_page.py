a6c97376dec47ea04e82178eea22fc9feb6d21c1
momofosho <54734022+momofosho@users.noreply.github.com>
2021-08-05 22:13:48 +0800

    Update influencer_page.py

--- influencer_page.py
+++ influencer_page.py
@@ -55,7 +55,7 @@ def influencerspage(state):
     #Getting all the hashtags in the table
     hash = df['hashtags'].tolist()
     hash = list(itertools.chain.from_iterable(hash))
-    hash = list(set(hash))
+    hash = list(set([i.lower() for i in hash]))
     col = df.columns.get_loc("hashtags")
     hashtag_df = pd.DataFrame() #initialise
     #select hashtag widget
